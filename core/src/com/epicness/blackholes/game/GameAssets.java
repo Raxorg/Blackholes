792a04d1bf7db725b3b7d84592489f2166b7a166
Luis Frontanilla <lrdolucho@gmail.com>
2021-09-20 18:56:26 -0400

    Small starting code for the renderer

--- core/src/com/epicness/blackholes/game/GameAssets.java
+++ core/src/com/epicness/blackholes/game/GameAssets.java
@@ -3,6 +3,7 @@ package com.epicness.blackholes.game;
 import com.epicness.fundamentals.assets.Assets;
 
 public class GameAssets extends Assets {
+
     @Override
     public void queueAssetLoading() {
 
--- core/src/com/epicness/blackholes/game/GameRenderer.java
+++ core/src/com/epicness/blackholes/game/GameRenderer.java
@@ -1,11 +1,20 @@
 package com.epicness.blackholes.game;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.epicness.blackholes.game.stuff.GameStuff;
 import com.epicness.fundamentals.renderer.Renderer;
 
 public class GameRenderer extends Renderer {
 
     @Override
     public void render() {
+        GameStuff stuff = (GameStuff) this.stuff;
 
+        ScreenUtils.clear(Color.BLACK);
+
+        spriteBatch.begin();
+
+        spriteBatch.end();
     }
 }
