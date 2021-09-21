3085efc18f237397e84a2381df22441278cc6c38
Luis Frontanilla <lrdolucho@gmail.com>
2021-09-21 19:07:53 -0400

    Connecting junk to the SpaceObjectHandler

--- core/src/com/epicness/blackholes/game/GameConstants.java
+++ core/src/com/epicness/blackholes/game/GameConstants.java
@@ -8,6 +8,7 @@ public class GameConstants {
     // Junk
     public static final int INITIAL_JUNK = 50;
     public static final float JUNK_SPAWN_RATE = 2f;
+    public static final float JUNK_SIZE = 25f;
 
     //Ships
     public static final float SHIP_SPEED = 10f;
--- core/src/com/epicness/blackholes/game/stuff/GameStuff.java
+++ core/src/com/epicness/blackholes/game/stuff/GameStuff.java
@@ -2,12 +2,14 @@ package com.epicness.blackholes.game.stuff;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.DelayedRemovalArray;
 import com.epicness.blackholes.game.GameAssets;
 import com.epicness.blackholes.game.stuff.blackholes.BlackHole;
 import com.epicness.fundamentals.stuff.Stuff;
 
+import static com.epicness.blackholes.game.GameConstants.INITIAL_JUNK;
 import static com.epicness.blackholes.game.stuff.blackholes.BlackHoleType.ALPHA;
 import static com.epicness.fundamentals.SharedConstants.CAMERA_HEIGHT;
 import static com.epicness.fundamentals.SharedConstants.CAMERA_WIDTH;
@@ -15,7 +17,7 @@ import static com.epicness.fundamentals.SharedConstants.CAMERA_WIDTH;
 public class GameStuff extends Stuff {
 
     public Ship playerShip, enemyShip;
-    public DelayedRemovalArray<SpaceObject> junk;
+    private DelayedRemovalArray<Junk> junks;
     private DelayedRemovalArray<BlackHole> blackHoles;
 
     @Override
@@ -27,14 +29,27 @@ public class GameStuff extends Stuff {
         playerShip.setPosition(new Vector2(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f));
         playerShip.setAngularVelocity(15f);
 
+        junks = new DelayedRemovalArray<>();
+        for (int i = 0; i < INITIAL_JUNK; i++) {
+            Junk junk = new Junk(sharedAssets.getWeirdShape(), sharedAssets.getPixel());
+            float randomX = MathUtils.random(0, CAMERA_WIDTH);
+            float randomY = MathUtils.random(0, CAMERA_HEIGHT);
+            junk.setPosition(new Vector2(randomX, randomY));
+            junks.add(junk);
+        }
+
         blackHoles = new DelayedRemovalArray<>();
         BlackHole blackHole = new BlackHole(
                 assets.getBlackHole(), assets.getBlackHoleGlow(),
                 assets.getInvertedBlackHole(), assets.getInvertedBlackHoleGlow(),
                 ALPHA);
-        blackHole.setPosition(CAMERA_WIDTH / 2f, CAMERA_HEIGHT / 2f);
+        blackHole.setPosition(new Vector2(CAMERA_WIDTH / 2f, CAMERA_HEIGHT / 2f));
         blackHole.setColor(Color.CHARTREUSE);
         blackHoles.add(blackHole);
+    }
+
+    public DelayedRemovalArray<Junk> getJunks() {
+        return junks;
     }
 
     public DelayedRemovalArray<BlackHole> getBlackHoles() {
