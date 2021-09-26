d4975ff607d92f0c4489f56c64f2a8ff95b4c2ea
Luis Frontanilla <lrdolucho@gmail.com>
2021-09-26 15:48:03 -0400

    Fixed html build

--- html/src/com/epicness/blackholes/client/HtmlLauncher.java
+++ html/src/com/epicness/blackholes/client/HtmlLauncher.java
@@ -3,20 +3,20 @@ package com.epicness.blackholes.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.epicness.blackholes.BlackholesGame;
+import com.epicness.blackholes.BlackHolesGame;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
-                // Fixed size application:
-                //return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        // Resizable application, uses available space in browser
+        return new GwtApplicationConfiguration(true);
+        // Fixed size application:
+        //return new GwtApplicationConfiguration(480, 320);
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new BlackholesGame();
-        }
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new BlackHolesGame();
+    }
 }
